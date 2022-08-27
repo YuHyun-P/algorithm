68bedc0a7d56dec17798b5565d296d44c5330b8a
박유현 <puh0128@gmail.com>
2022-08-27 21:03:26 +0900

    Time: 98 ms (59.50%), Space: 45.4 MB (45.41%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (!root) return 0;
+  
+  let max = 1;
+  const dfs = (node, level) => {
+    max = Math.max(level, max);
+    if (node.left) dfs(node.left, level + 1);
+    if (node.right) dfs(node.right, level + 1);
+  };
+  dfs(root, max);
+  return max;
+};
