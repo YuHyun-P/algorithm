5d3e2bc13ba69aa7c4a44833975866adad21f262
박유현 <puh0128@gmail.com>
2022-08-29 00:07:28 +0900

    Time: 137 ms (25.57%), Space: 45.3 MB (44.32%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.js
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.js
@@ -4,35 +4,25 @@
  */
 var diagonalSort = function(mat) {
   const [m, n] = [mat.length, mat[0].length];
-  const diagonalNum = m + n - 1;
-  const diagonalPaths = [];
+  const diagonalPaths = {};
   const afterMat = Array.from({length: m}, () => Array.from({length: n}).fill(0));
   
-  const getDiagonalPath = (start) => {
-    let [curRow, curCol] = start;
-    const diagonalPath = [];
-    
-    while (curRow < m && curCol < n) {
-      diagonalPath.push([curRow++, curCol++]);
+  mat.forEach((row, rowIndex) => row.forEach((col, colIndex) => {
+    const key = colIndex - rowIndex;
+    const num = mat[rowIndex][colIndex];
+    if (diagonalPaths[key]) {
+      diagonalPaths[key].push(num);
+    } else {
+      diagonalPaths[key] = [num];
     }
-    return diagonalPath;
-  }
+  }));
   
-  Array.from({length: m}).forEach((_, index) => {
-    const start = [m - index - 1, 0];
-    diagonalPaths.push(getDiagonalPath(start));
-  })
-  Array.from({length: n - 1}).forEach((_, index) => {
-    const start = [0, index + 1];
-    diagonalPaths.push(getDiagonalPath(start));
-  })
+  Object.keys(diagonalPaths).forEach((key) => diagonalPaths[key].sort((a, b) => b - a));
   
-  diagonalPaths.forEach((path) => {
-    const beforeDiagonal = path.map(([i, j]) => mat[i][j]);
-    const afterDiagonal = beforeDiagonal.sort((a, b) => a - b);
-    
-    path.forEach(([i, j], index) => afterMat[i][j] = afterDiagonal[index]);
-  })
-  
+  afterMat.forEach((row, rowIndex) => row.forEach((col, colIndex) => {
+    const key = colIndex - rowIndex;
+    const num = diagonalPaths[key].pop();
+    afterMat[rowIndex][colIndex] = num;
+  }));
   return afterMat;
 };
