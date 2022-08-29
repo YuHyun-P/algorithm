82838f8f079b84d3f82f52d383edb9cb7d1336f7
박유현 <puh0128@gmail.com>
2022-08-29 20:10:22 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 66.67 ms, Memory: 176 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
@@ -0,0 +1,35 @@
+function solution(board, skill) {
+    const [m, n] = [board.length, board[0].length];
+    const prefixSum = Array.from({length: m + 1}, () => new Array(n + 1).fill(0));
+    
+    // skill.length = M, O(M)
+    skill.forEach(([type, r1, c1, r2, c2, degree]) => {
+        const degreeWithSign = type === 1 ? degree * -1 : degree;
+        prefixSum[r1][c1] += degreeWithSign;
+        prefixSum[r1][c2 + 1] += degreeWithSign * -1;
+        prefixSum[r2 + 1][c2 + 1] += degreeWithSign;
+        prefixSum[r2 + 1][c1] += degreeWithSign * -1;
+    });
+    
+    // 행방향 누적합
+    for (let r=0; r<m; r++) {
+        for (let c=1; c<n; c++) {
+            prefixSum[r][c] += prefixSum[r][c-1];
+        }
+    }
+    
+    // 열방향 누적합
+    for (let r=1; r<m; r++) {
+        for (let c=0; c<n; c++) {
+            prefixSum[r][c] += prefixSum[r-1][c];
+        }
+    }
+    
+    let undestroyCount = 0;
+    board.forEach((row, r) => row.forEach((num, c) => {
+        const afterSkillNum = num + prefixSum[r][c];
+        if (afterSkillNum > 0) undestroyCount++;
+    }))
+    
+    return undestroyCount;
+}
