8958fdb99711203f94e8fea5bfd6e4ce501ef498
박유현 <puh0128@gmail.com>
2022-09-06 11:51:18 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 15.65 ms, Memory: 46.5 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,40 +1,23 @@
 function solution(queue1, queue2) {
-    const LENGTH = queue1.length;
-    const prefixQ1 = [];
-    const prefixQ2 = [];
-    queue1.forEach((num, index) => prefixQ1.push(index === 0 ? BigInt(num) : BigInt(num) + BigInt(prefixQ1[index-1])));
-    queue2.forEach((num, index) => prefixQ2.push(index === 0 ? BigInt(num) : BigInt(num) + BigInt(prefixQ2[index-1])));
-
-    const TARGET = (prefixQ1.at(-1) + prefixQ2.at(-1)) / 2n;
-    if (TARGET === prefixQ1.at(-1)) return 0;
+    let q1Total = queue1.reduce((acc, cur) => acc + cur, 0),
+          q2Total = queue2.reduce((acc, cur) => acc + cur, 0);
+    const TARGET = (q1Total + q2Total) / 2;
     
-    let minusIndex = null;
-    let plusIndex = 0;
-    
-    const [firstPrefixQ, secondPrefixQ] = prefixQ1.at(-1) < prefixQ2.at(-1) ? [prefixQ1, prefixQ2] : [prefixQ2, prefixQ1];
-    
-    while (minusIndex - LENGTH < plusIndex && plusIndex < LENGTH) {
-        let minusNum;
-        
-        if (minusIndex === null) {
-            minusNum = 0n;
-        } else if (minusIndex < LENGTH) {
-            minusNum = firstPrefixQ[minusIndex];
+    const concatQueue = [...queue1, ...queue2];
+    let q1Index = 0,
+        q2Index = queue2.length;
+    while (q1Index < q2Index) {
+        if (q1Total === q2Total) {
+            return q1Index + q2Index - queue2.length;
+        } else if (q1Total < q2Total) {
+            q1Total += concatQueue[q2Index];
+            q2Total -= concatQueue[q2Index];
+            q2Index++;
         } else {
-            minusNum = firstPrefixQ.at(-1) + secondPrefixQ[minusIndex - LENGTH];
-        }
-        
-        const curTotal = firstPrefixQ.at(-1) + secondPrefixQ[plusIndex] - minusNum;
-        
-        if (curTotal < TARGET) {
-            plusIndex++;
-        } else if (curTotal === TARGET) {
-            if (minusIndex === null) return plusIndex + 1;
-            return minusIndex + 1 + plusIndex + 1;
-        } else {
-            minusIndex = minusIndex === null ? 0 : minusIndex + 1;
+            q1Total -= concatQueue[q1Index];
+            q2Total += concatQueue[q1Index];
+            q1Index++;
         }
     }
-    
     return -1;
 }
