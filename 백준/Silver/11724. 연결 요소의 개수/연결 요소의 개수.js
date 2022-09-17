82a7f461aa71a0d29a0e17529120eec2665f5a5b
박유현 <puh0128@gmail.com>
2022-09-17 23:57:00 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 856 ms, Memory: 100496 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -1,51 +1,6 @@
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
-
-  enqueue(value) {
-    const newNode = new Node(value);
-    if (this.isEmpty()) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-
-    this.size++;
-  }
-
-  dequeue() {
-    if (this.isEmpty()) return undefined;
-
-    const value = this.head.value;
-    this.head = this.head.next;
-    if (this.head === null) {
-      this.tail = null;
-    }
-
-    this.size--;
-    return value;
-  }
-
-  isEmpty() {
-    return this.size === 0;
-  }
-}
-
 const [N, _] = input[0].trim().split(" ").map(Number);
 
 const graph = Array.from(Array(N + 1), () => []);
@@ -57,22 +12,22 @@ for (let line = 1; line < input.length; line++) {
 
 let count = 0;
 const visited = Array(N + 1).fill(false);
-const queue = new Queue();
+const queue = [];
 
 for (let start = 1; start < N + 1; start++) {
   if (visited[start]) continue;
 
-  queue.enqueue(start);
+  queue.push(start);
   visited[start] = true;
 
-  while (!queue.isEmpty()) {
-    const cur = queue.dequeue();
+  while (queue.length > 0) {
+    const cur = queue.shift();
 
     graph[cur].forEach((next) => {
       if (visited[next]) return;
 
       visited[next] = true;
-      queue.enqueue(next);
+      queue.push(next);
     });
   }
 
