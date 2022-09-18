3ad3960012b26b1cee4dd203aa3fdb6911df89c3
박유현 <puh0128@gmail.com>
2022-09-18 11:41:24 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 58.41 ms, Memory: 176 MB -BaekjoonHub

--- 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
+++ 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
@@ -1,40 +1,31 @@
 function solution(board, skill) {
-  const [m, n] = [board.length, board[0].length];
-  const prefixSum = Array.from({ length: m + 1 }, () =>
-    new Array(n + 1).fill(0)
-  );
-
-  // 범위 cell에 누적할 값 채우기
-  skill.forEach(([type, r1, c1, r2, c2, degree]) => {
-    const degreeWithSign = type === 1 ? degree * -1 : degree;
-    prefixSum[r1][c1] += degreeWithSign;
-    prefixSum[r1][c2 + 1] += degreeWithSign * -1;
-    prefixSum[r2 + 1][c2 + 1] += degreeWithSign;
-    prefixSum[r2 + 1][c1] += degreeWithSign * -1;
-  });
-
-  // 행방향 누적합
-  for (let r = 0; r < m; r++) {
-    for (let c = 1; c < n; c++) {
-      prefixSum[r][c] += prefixSum[r][c - 1];
+    const N = board.length;
+    const M = board[0].length;
+    
+    const prefixSum = Array.from(Array(N + 1), () => Array(M + 1).fill(0));
+    skill.forEach(([type, r1, c1, r2, c2, degree]) => {
+        const signDegree = type === 1 ? -degree : degree;
+        prefixSum[r1][c1] += signDegree;
+        prefixSum[r1][c2 + 1] -= signDegree;
+        
+        prefixSum[r2 + 1][c1] -= signDegree;
+        prefixSum[r2 + 1][c2 + 1] += signDegree;
+    });
+    
+    for (let r = 0; r < N + 1; r++) {
+        for (let c = 1; c < M + 1; c++) {
+            prefixSum[r][c] += prefixSum[r][c - 1];
+        }
     }
-  }
-
-  // 열방향 누적합
-  for (let r = 1; r < m; r++) {
-    for (let c = 0; c < n; c++) {
-      prefixSum[r][c] += prefixSum[r - 1][c];
+    
+    for (let r = 1; r < N + 1; r++) {
+        for (let c = 0; c < M + 1; c++) {
+            prefixSum[r][c] += prefixSum[r - 1][c];
+        }
     }
-  }
-
-  // 파괴되지 않은 건물 세기
-  let undestroyCount = 0;
-  board.forEach((row, r) =>
-    row.forEach((num, c) => {
-      const afterSkillNum = num + prefixSum[r][c];
-      if (afterSkillNum > 0) undestroyCount++;
-    })
-  );
-
-  return undestroyCount;
+    
+    let count = 0;
+    board.forEach((row, r) => row.forEach((cell, c) => cell + prefixSum[r][c] > 0 && count++ ))
+    
+    return count;
 }
