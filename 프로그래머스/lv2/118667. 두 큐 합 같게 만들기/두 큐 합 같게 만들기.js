798d0d354410786bfe188bdc3cb5b3ac52122f78
박유현 <puh0128@gmail.com>
2022-09-21 11:09:43 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 19.44 ms, Memory: 49.4 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,23 +1,30 @@
 function solution(queue1, queue2) {
-    let q1Total = queue1.reduce((acc, cur) => acc + cur, 0),
-          q2Total = queue2.reduce((acc, cur) => acc + cur, 0);
-    const TARGET = (q1Total + q2Total) / 2;
+    const queue = [...queue1, ...queue2];
     
-    const concatQueue = [...queue1, ...queue2];
-    let q1Index = 0,
-        q2Index = queue2.length;
-    while (q1Index < q2Index) {
-        if (q1Total === q2Total) {
-            return q1Index + q2Index - queue2.length;
-        } else if (q1Total < q2Total) {
-            q1Total += concatQueue[q2Index];
-            q2Total -= concatQueue[q2Index];
-            q2Index++;
+    const sum = (acc, cur) => acc + cur;
+    const target = queue.reduce(sum, 0) / 2;
+    let queue1Total = queue1.reduce(sum, 0);
+    let queue2Total = queue2.reduce(sum, 0);
+    
+    let queue1Index = 0;
+    let queue2Index = queue1.length;
+    let count = 0;
+    while (queue1Index < queue2Index && queue2Index < queue.length) {
+        if (queue1Total > queue2Total) {
+            const popNum = queue[queue1Index];
+            queue1Total -= popNum;
+            queue2Total += popNum;
+            queue1Index++;
+        } else if (queue1Total === queue2Total){
+            break;
         } else {
-            q1Total -= concatQueue[q1Index];
-            q2Total += concatQueue[q1Index];
-            q1Index++;
+            const popNum = queue[queue2Index];
+            queue2Total -= popNum;
+            queue1Total += popNum;
+            queue2Index++;
         }
+        count++;
     }
-    return -1;
+    
+    return queue1Total === queue2Total ? count : -1;
 }
