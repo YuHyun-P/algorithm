357f21e2f167ba6b98280510d22a18d6a5b8342a
박유현 <puh0128@gmail.com>
2022-12-28 16:00:03 +0900

    [Gold II] Title: 2048 (Easy), Time: 564 ms, Memory: 15788 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.js
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.js
@@ -2,108 +2,69 @@ const fs = require("fs");
 const path = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
 const input = fs.readFileSync(path).toString().trim().split("\n");
 
-const N = Number(input.shift().trim());
+function solution(N, board) {
+  const LIMIT = 5;
+  let max = 0;
+
+  const rotate = (board) => {
+    const rotated = Array.from(Array(N), () => Array(N).fill(0));
+    for (let row = 0; row < N; row++) {
+      for (let col = 0; col < N; col++) {
+        rotated[col][N - 1 - row] = board[row][col];
+      }
+    }
+
+    for (let row = 0; row < N; row++) {
+      board[row] = rotated[row];
+    }
+  };
+  const tilt = (board) => {
+    for (let row = 0; row < N; row++) {
+      const tilted = Array(N).fill(0);
+      let tiltedCol = N - 1;
+
+      for (let col = N - 1; 0 <= col; col--) {
+        if (board[row][col] === 0) {
+          continue;
+        }
+
+        if (board[row][col] === tilted[tiltedCol]) {
+          tilted[tiltedCol] *= 2;
+          tiltedCol -= 1;
+          continue;
+        }
+
+        if (tilted[tiltedCol]) {
+          tiltedCol -= 1;
+        }
+
+        tilted[tiltedCol] = board[row][col];
+      }
+
+      board[row] = tilted;
+    }
+  };
+
+  for (let path = 0; path < 4 ** LIMIT; path++) {
+    let curBoard = board.map((row) => [...row]);
+    let curPath = path;
+
+    for (let iter = 0; iter < LIMIT; iter++) {
+      const direction = curPath % 4;
+      curPath = Math.floor(curPath / 4);
+
+      for (let rotateIter = direction; rotateIter > 0; rotateIter--) {
+        rotate(curBoard);
+      }
+
+      tilt(curBoard);
+    }
+
+    max = Math.max(max, ...curBoard.flat());
+  }
+  return max;
+}
+
+const N = Number(input.shift());
 const board = input.map((row) => row.trim().split(" ").map(Number));
-
-const LIMIT = 5;
-let max = 0;
-
-const merge = (direction, board) => {
-  // direction 0: Right, 1: Down, 2: Left, 3: Up
-
-  switch (direction) {
-    case 0:
-      mergeRow(1, board);
-      break;
-    case 1:
-      mergeCol(1, board);
-      break;
-    case 2:
-      mergeRow(-1, board);
-      break;
-    case 3:
-      mergeCol(-1, board);
-      break;
-    default:
-      throw new Error(`Invalid direction ${direction}`);
-  }
-};
-
-const mergeRow = (direction, board) => {
-  for (let row = 0; row < N; row++) {
-    const rowWithoutZero = board[row].filter((num) => num !== 0);
-    const stack =
-      direction === 1 ? [...rowWithoutZero] : [...rowWithoutZero.reverse()];
-    const mergedRow = [];
-    let last = 0;
-
-    while (stack.length) {
-      const num = stack.pop();
-      if (last > 0 && num === last) {
-        mergedRow.pop();
-        mergedRow.push(num * 2);
-        last = 0;
-        continue;
-      }
-      mergedRow.push(num);
-      last = num;
-    }
-    while (mergedRow.length < N) {
-      mergedRow.push(0);
-    }
-    board[row] = direction === 1 ? mergedRow.reverse() : mergedRow;
-  }
-};
-const mergeCol = (direction, board) => {
-  for (let col = 0; col < N; col++) {
-    const colWithoutZero = board
-      .map((row) => row[col])
-      .filter((num) => num !== 0);
-    const stack = direction === 1 ? colWithoutZero : colWithoutZero.reverse();
-    const mergedCol = [];
-    let last = 0;
-
-    while (stack.length) {
-      const num = stack.pop();
-      if (last > 0 && num === last) {
-        mergedCol.pop();
-        mergedCol.push(num * 2);
-        last = 0;
-        continue;
-      }
-      mergedCol.push(num);
-      last = num;
-    }
-    while (mergedCol.length < N) {
-      mergedCol.push(0);
-    }
-    if (direction === 1) {
-      mergedCol.reverse();
-    }
-    mergedCol.forEach((num, row) => {
-      board[row][col] = num;
-    });
-  }
-};
-
-const dfs = (level, board) => {
-  max = Math.max(max, ...board.flat());
-  if (level === LIMIT) {
-    return;
-  }
-
-  const before = board.map((row) => row.join(" ")).join("\n");
-  for (let direction = 0; direction < 4; direction++) {
-    merge(direction, board);
-    const after = board.map((row) => row.join(" ")).join("\n");
-
-    if (before === after) {
-      continue;
-    }
-
-    dfs(level + 1, board);
-    board = before.split("\n").map((row) => row.split(" ").map(Number));
-  }
-};
-dfs(0, board);
-console.log(max);
+console.log(solution(N, board));
