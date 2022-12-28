0e46182c897dcd181910d9e803ed4e00391086db
박유현 <puh0128@gmail.com>
2022-12-28 17:51:29 +0900

    [Gold V] Title: 치킨 배달, Time: 236 ms, Memory: 13360 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.js
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.js
@@ -3,81 +3,48 @@ const path = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
 const input = fs.readFileSync(path).toString().trim().split("\n");
 
 function solution(N, M, village) {
-  const HOUSE = 1;
-  const dr = [0, 1, 0, -1];
-  const dc = [1, 0, -1, 0];
-  class Queue {
-    array = [];
-    head = 0;
-    tail = 0;
-    enqueue(value) {
-      this.array[this.tail] = value;
-      this.tail += 1;
-    }
-    dequeue() {
-      const value = this.array[this.head];
-      this.head += 1;
-      return value;
-    }
-    isEmpty() {
-      return this.tail - this.head === 0;
-    }
-  }
+  const chicken = [];
+  const house = [];
 
-  const chicken = (() => {
-    const chicken = [];
-    for (let row = 0; row < N; row++) {
-      for (let col = 0; col < N; col++) {
-        if (village[row][col] === 2) {
-          chicken.push([chicken.length, row, col]);
-        }
+  for (let row = 0; row < N; row++) {
+    for (let col = 0; col < N; col++) {
+      switch (village[row][col]) {
+        case 1:
+          house.push([row, col]);
+          break;
+        case 2:
+          chicken.push([row, col]);
+          break;
       }
     }
-    return chicken;
-  })();
+  }
 
   const visited = Array(chicken.length).fill(false);
   let minDistance = Infinity;
 
-  const outOfBound = (r, c) => r < 0 || N <= r || c < 0 || N <= c;
-  const calculateDistance = (index, row, col) => {
-    const [_, chickenRow, chickenCol] = chicken[index];
-    return Math.abs(row - chickenRow) + Math.abs(col - chickenCol);
-  };
-  const bfs = (chicken) => {
+  const getTotalDistance = (chicken) => {
     let totalDistance = 0;
-    const visited = Array.from(Array(N), () => Array(N).fill(false));
-    const queue = new Queue();
-    chicken.forEach((value) => {
-      const [_, row, col] = value;
-      queue.enqueue(value);
-      visited[row][col] = true;
+
+    house.forEach(([houseRow, houseCol]) => {
+      let distance = Infinity;
+
+      chicken.forEach(([chickenRow, chickenCol]) => {
+        distance = Math.min(
+          distance,
+          Math.abs(houseRow - chickenRow) + Math.abs(houseCol - chickenCol)
+        );
+      });
+
+      totalDistance += distance;
     });
-
-    while (!queue.isEmpty()) {
-      const [index, row, col] = queue.dequeue();
-
-      for (let direction = 0; direction < 4; direction++) {
-        const nextRow = row + dr[direction];
-        const nextCol = col + dc[direction];
-        if (outOfBound(nextRow, nextCol) || visited[nextRow][nextCol]) {
-          continue;
-        }
-
-        visited[nextRow][nextCol] = true;
-        if (village[nextRow][nextCol] === HOUSE) {
-          totalDistance += calculateDistance(index, nextRow, nextCol);
-        }
-
-        queue.enqueue([index, nextRow, nextCol]);
-      }
-    }
 
     return totalDistance;
   };
   const backtracking = (selected, count) => {
     if (count === M) {
-      const totalDistance = bfs(chicken.filter((_, index) => visited[index]));
+      const totalDistance = getTotalDistance(
+        chicken.filter((_, index) => visited[index])
+      );
       minDistance = Math.min(minDistance, totalDistance);
       return;
     }
