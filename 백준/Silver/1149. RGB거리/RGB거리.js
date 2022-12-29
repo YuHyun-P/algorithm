4407c394f39efe4959cc7f547abb65b5ec077972
박유현 <puh0128@gmail.com>
2022-12-29 18:00:24 +0900

    [Silver I] Title: RGB거리, Time: 156 ms, Memory: 9624 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.js
+++ 백준/Silver/1149. RGB거리/RGB거리.js
@@ -2,35 +2,17 @@ const fs = require("fs");
 const path = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
 const input = fs.readFileSync(path).toString().trim().split("\n");
 
-const N = Number(input.shift().trim());
-const rgbCost = input.map((line) => line.trim().split(" ").map(Number));
-const RGB = 3;
-
-for (let house = 1; house < N; house++) {
-  const prevHouse = house - 1;
-  for (let rgb = 0; rgb < RGB; rgb++) {
-    switch (rgb) {
-      case 0:
-        rgbCost[house][rgb] += Math.min(
-          rgbCost[prevHouse][1],
-          rgbCost[prevHouse][2]
-        );
-        break;
-      case 1:
-        rgbCost[house][rgb] += Math.min(
-          rgbCost[prevHouse][0],
-          rgbCost[prevHouse][2]
-        );
-        break;
-      case 2:
-        rgbCost[house][rgb] += Math.min(
-          rgbCost[prevHouse][0],
-          rgbCost[prevHouse][1]
-        );
-        break;
-      default:
-        throw new Error("Invalid rgb");
-    }
+function solution(N, house) {
+  const dp = house.map((rgb) => [...rgb]);
+  for (let index = 1; index < N; index++) {
+    dp[index][0] += Math.min(dp[index - 1][1], dp[index - 1][2]);
+    dp[index][1] += Math.min(dp[index - 1][0], dp[index - 1][2]);
+    dp[index][2] += Math.min(dp[index - 1][0], dp[index - 1][1]);
   }
+
+  return Math.min(dp[N - 1][0], dp[N - 1][1], dp[N - 1][2]);
 }
-console.log(Math.min(...rgbCost[N - 1]));
+
+const N = Number(input.shift().trim());
+const house = input.map((row) => row.trim().split(" ").map(Number));
+console.log(solution(N, house));
