090feeafdd7054f4480e4328cf23a32388e95b2a
박유현 <puh0128@gmail.com>
2023-01-02 16:11:30 +0900

    [Silver I] Title: 카잉 달력, Time: 240 ms, Memory: 10680 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.js
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.js
@@ -2,31 +2,36 @@ const fs = require("fs");
 const path = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
 const input = fs.readFileSync(path).toString().trim().split("\n");
 
-const totalTestCase = Number(input.shift().trim());
-const questions = input.map((line) => line.trim().split(" ").map(Number));
-
-const lcm = (a, b) => {
-  const gcd = (a, b) => {
-    if (b === 0) {
-      return a;
+function solution(testCase) {
+  const getGCD = (M, N) => {
+    if (N === 0) {
+      return M;
     }
-    return gcd(b, a % b);
+    return getGCD(N, M % N);
+  };
+  const getLCM = (M, N) => {
+    const gcd = getGCD(M, N);
+    return gcd * (M / gcd) * (N / gcd);
   };
 
-  const gcdNM = gcd(a, b);
-  return gcdNM * (a / gcdNM) * (b / gcdNM);
-};
-
-const getYear = ([M, N, x, y]) => {
-  const maxYear = lcm(M, N);
-  let year = -1;
-  for (let curYear = x; curYear <= maxYear; curYear += M) {
-    if ((curYear % N || N) === y) {
-      year = curYear;
-      break;
+  const getYear = ([M, N, x, y]) => {
+    if (y === N) {
+      y = 0;
     }
-  }
-  return year;
-};
 
-console.log(questions.map(getYear).join("\n"));
+    const max = getLCM(M, N);
+    for (let composite = 0; composite * M + x <= max; composite++) {
+      const cur = composite * M + x;
+
+      if (cur % N === y) {
+        return cur;
+      }
+    }
+    return -1;
+  };
+  return testCase.map(getYear);
+}
+
+const T = Number(input.shift().trim());
+const testCase = input.map((row) => row.trim().split(" ").map(Number));
+console.log(solution(testCase).join("\n"));
