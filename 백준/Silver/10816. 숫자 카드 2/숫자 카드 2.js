c25a6c8d357d150cd09f1c06a1f989cf488e85e8
박유현 <puh0128@gmail.com>
2023-01-03 14:31:15 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1224 ms, Memory: 138796 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -3,36 +3,30 @@ const path = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
 const input = fs.readFileSync(path).toString().trim().split("\n");
 
 function solution(seqN, seqM) {
-  seqN.sort((a, b) => a - b);
-  const countSeqN = (() => {
-    const map = new Map();
-    seqN.forEach((num) => {
-      map.set(num, (map.get(num) ?? 0) + 1);
-    });
-    return [...map];
-  })();
-
-  const getCount = (left, right, sequence, k) => {
-    if (left >= right) {
-      return 0;
+  const getLowerIndex = (left, right, sequence, k) => {
+    if (left === right) {
+      return left;
     }
 
     const mid = Math.floor((left + right) / 2);
-    const diff = Math.sign(sequence[mid][0] - k);
+    const diff = Math.sign(sequence[mid] - k);
     switch (diff) {
-      case 0:
-        return sequence[mid][1];
       case -1:
-        return getCount(mid + 1, right, sequence, k);
+        return getLowerIndex(mid + 1, right, sequence, k);
+      case 0:
       case 1:
-        return getCount(left, mid, sequence, k);
+        return getLowerIndex(left, mid, sequence, k);
     }
   };
-
-  return seqM.map((num) => getCount(0, countSeqN.length, countSeqN, num));
+  seqN.sort((a, b) => a - b);
+  return seqM.map(
+    (num) =>
+      getLowerIndex(0, seqN.length, seqN, num + 1) -
+      getLowerIndex(0, seqN.length, seqN, num)
+  );
 }
 
 const [seqN, seqM] = [input[1], input[3]].map((row) =>
   row.trim().split(" ").map(Number)
 );
-console.log(solution(seqN, seqM).join(" "));
+console.log(solution(seqN, seqM).join("\n"));
