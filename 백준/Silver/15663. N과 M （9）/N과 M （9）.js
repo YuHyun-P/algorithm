95ee052a6675647f4f844f60eabbd3504e5d6005
박유현 <puh0128@gmail.com>
2023-01-16 15:38:30 +0900

    [Silver II] Title: N과 M (9), Time: 188 ms, Memory: 13424 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.js
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.js
@@ -4,30 +4,35 @@ const input = fs.readFileSync(path).toString().trim().split("\n");
 
 function solution(N, M, sequence) {
   sequence.sort((a, b) => a - b);
-  const answer = new Set();
+  const answer = [];
+  const path = [];
   const visited = Array(N).fill(false);
 
-  const backtracking = (path, level) => {
+  const backtracking = (level) => {
     if (level === M) {
-      answer.add(path.trimEnd());
+      answer.push(path.join(" "));
       return;
     }
 
+    let tmp = 0;
     for (let index = 0; index < N; index++) {
-      if (visited[index]) {
+      if (visited[index] || tmp === sequence[index]) {
         continue;
       }
 
       visited[index] = true;
-      backtracking(path + `${sequence[index]} `, level + 1);
+      path[level] = tmp = sequence[index];
+      backtracking(level + 1);
       visited[index] = false;
     }
   };
 
-  backtracking("", 0);
+  backtracking(0);
   return [...answer].join("\n");
 }
 
 const [N, M] = input.shift().trim().split(" ").map(Number);
 const sequence = input[0].trim().split(" ").map(Number);
 console.log(solution(N, M, sequence));
+
+// reference https://github.com/encrypted-def/basic-algo-lecture/blob/master/0x0C/solutions/15663.cpp
