0d89a77a76f8c246d3185d7a79c8e2bd52fef305
박유현 <puh0128@gmail.com>
2023-01-18 14:58:20 +0900

    [Silver III] Title: 퇴사, Time: 128 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.js
+++ 백준/Silver/14501. 퇴사/퇴사.js
@@ -3,35 +3,22 @@ const path = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
 const input = fs.readFileSync(path).toString().trim().split("\n");
 
 function solution(N, meeting) {
-  const prev = Array(N + 1).fill(-1);
   const max = Array(N + 1).fill(0);
 
-  for (let index = 0; index < meeting.length; index++) {
-    if (max[index] < (max[index - 1] ?? 0)) {
-      prev[index] = prev[index - 1];
-      max[index] = max[index - 1];
-    }
-
+  for (let index = N - 1; 0 <= index; index--) {
     const [t, p] = meeting[index];
-    if (t === 1) {
-      prev[index] = index;
-      max[index] += p;
-      continue;
+    if (t + index < N + 1) {
+      max[index] = Math.max(max[index + 1], (max[t + index] ?? 0) + p);
+    } else {
+      max[index] = max[index + 1];
     }
-
-    const endT = index + t;
-    const sumP = max[index] + p;
-    if (N < endT || sumP < max[endT]) {
-      continue;
-    }
-
-    prev[endT] = index;
-    max[endT] = sumP;
   }
 
-  return Math.max(max[N], max[N - 1]);
+  return Math.max(...max);
 }
 
 const N = Number(input.shift().trim());
 const meeting = input.map((row) => row.trim().split(" ").map(Number));
 console.log(solution(N, meeting));
+
+// reference https://github.com/encrypted-def/basic-algo-lecture/blob/master/0x10/solutions/14501.cpp
