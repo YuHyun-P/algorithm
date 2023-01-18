d5828c55a2811c1ccd06ba599f1c38ea9c8fd308
박유현 <puh0128@gmail.com>
2023-01-18 15:24:24 +0900

    [Gold V] Title: 퇴사 2, Time: 1724 ms, Memory: 178508 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.js
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.js
@@ -4,19 +4,23 @@ const input = fs.readFileSync(path).toString().trim().split("\n");
 
 function solution(n, meeting) {
   const max = Array(n + 1).fill(0);
-  for (let index = n - 1; 0 <= index; index--) {
-    const [t, p] = meeting[index];
+  let totalMax = 0;
 
-    if (index + t < n + 1) {
-      max[index] = Math.max(max[index + 1], max[index + t] + p);
-    } else {
-      max[index] = max[index + 1];
+  for (let index = n - 1; 0 <= index; index--) {
+    const [t, p] = meeting[index].trim().split(" ").map(Number);
+    max[index] = max[index + 1];
+
+    if (index + t < n + 1 && max[index] < max[index + t] + p) {
+      max[index] = max[index + t] + p;
+    }
+
+    if (totalMax < max[index]) {
+      totalMax = max[index];
     }
   }
 
-  return Math.max(...max);
+  return totalMax;
 }
 
 const n = Number(input.shift().trim());
-const meeting = input.map((row) => row.trim().split(" ").map(Number));
-console.log(solution(n, meeting));
+console.log(solution(n, input));
