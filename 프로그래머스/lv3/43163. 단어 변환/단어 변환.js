a227201b3acb5371b6178ac9caf25ecc061c9467
박유현 <puh0128@gmail.com>
2023-04-14 22:50:09 +0900

    [level 3] Title: 단어 변환, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -1,22 +1,55 @@
 function solution(begin, target, words) {
-    const visited = Array(words.length).fill(false);
+    const vertices = [begin, ...words];
+    const WORD_LENGTH = begin.length;
     
-    const countDiff = (wordA, wordB) => [...wordA].filter((char, index) => wordB[index] !== char).length;
-    const dfs = (cur) => {
-        if (cur === target) return 0;
-        
-        let min = Infinity;
-        for (const [index, next] of words.entries()) {
-            if (visited[index]) continue;
-            if (countDiff(cur, next) !== 1) continue;
+    const graph = new Map(Array.from(
+        vertices,
+        (word) => [word, { level: Infinity, edges: [] }]
+    ));
+    
+    for (let i = 0; i < vertices.length - 1; i++) {
+        for (let j = i + 1; j < vertices.length; j++) {
+            const word1 = vertices[i],
+                  word2 = vertices[j];
             
-            visited[index] = true;
-            min = Math.min(min, dfs(next) + 1);
-            visited[index] = false;
+            if (word1 === word2) continue;
+            
+            let diffCount = 0;
+            for (let index = 0; index < WORD_LENGTH; index++) {
+                if (diffCount > 1) break;
+                if (word1[index] === word2[index]) continue;
+                
+                diffCount++;
+            }
+            
+            if (diffCount > 1) continue;
+            
+            graph.get(word1).edges.push(word2);
+            graph.get(word2).edges.push(word1);
         }
-        return min;
-    };
+    }
     
-    const min = dfs(begin);
-    return min !== Infinity ? min : 0;
+    const queue = [begin];
+    const visited = new Set();
+    graph.set(begin, {...graph.get(begin), level: 0});
+    visited.add(begin);
+    
+    while (queue.length > 0) {
+        const vertex = queue.shift();
+        
+        const { level: curLevel, edges } = graph.get(vertex);
+        edges.forEach((nextVertex) => {
+            if (visited.has(nextVertex)) return;
+            
+            queue.push(nextVertex);
+            visited.add(nextVertex);
+            console.log(nextVertex, curLevel + 1);
+            graph.set(nextVertex, {...graph.get(nextVertex), level: curLevel + 1})
+        })
+    }
+    
+    if (graph.get(target) === undefined) return 0;
+    const answer = graph.get(target).level;
+    
+    return answer === Infinity ? 0 : answer;
 }
