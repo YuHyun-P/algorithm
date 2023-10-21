fa45fdb3c0b6838ef03ecb0e7acc097a4a60fd94
박유현 <puh0128@gmail.com>
2023-10-21 14:35:29 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 644 ms, Memory: 88632 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.js
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.js
@@ -3,101 +3,122 @@ const path = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
 const input = fs.readFileSync(path).toString().trim().split("\n");
 
 class Heap {
-  constructor(compare) {
+  constructor(compareFn) {
+    this.compareFn = compareFn;
     this.heap = [];
-    this.compare = compare;
   }
+
   getLeftChildIndex(parentIndex) {
     return parentIndex * 2 + 1;
   }
+
   getRightChildIndex(parentIndex) {
     return parentIndex * 2 + 2;
   }
+
   getParentIndex(childIndex) {
     return Math.floor((childIndex - 1) / 2);
   }
+
   hasLeftChild(parentIndex) {
     return this.getLeftChildIndex(parentIndex) < this.heap.length;
   }
+
   hasRightChild(parentIndex) {
     return this.getRightChildIndex(parentIndex) < this.heap.length;
   }
+
   hasParent(childIndex) {
     return 0 <= this.getParentIndex(childIndex);
   }
+
   push(value) {
     this.heap.push(value);
     this.heapifyUp();
   }
-  heapifyUp() {
-    let cur = this.heap.length - 1;
-    while (
-      this.hasParent(cur) &&
-      this.compare(this.heap[cur], this.heap[this.getParentIndex(cur)]) < 0
-    ) {
-      this.swap(cur, this.getParentIndex(cur));
-      cur = this.getParentIndex(cur);
-    }
-  }
+
   pop() {
-    if (this.isEmpty()) {
-      return null;
-    }
-    if (this.heap.length === 1) {
+    if (this.size() <= 1) {
       return this.heap.pop();
     }
+
     const value = this.heap[0];
     this.heap[0] = this.heap.pop();
     this.heapifyDown();
     return value;
   }
+
+  top() {
+    return this.heap[0];
+  }
+
+  heapifyUp() {
+    let cur = this.size() - 1;
+    while (
+      this.hasParent(cur) &&
+      this.compareFn(this.heap[cur], this.heap[this.getParentIndex(cur)]) < 0
+    ) {
+      const next = this.getParentIndex(cur);
+      this.swap(cur, next);
+      cur = next;
+    }
+  }
+
   heapifyDown() {
     let cur = 0;
     while (this.hasLeftChild(cur)) {
       const nextIsRight =
         this.hasRightChild(cur) &&
-        this.compare(
+        this.compareFn(
           this.heap[this.getRightChildIndex(cur)],
           this.heap[this.getLeftChildIndex(cur)]
         ) < 0;
 
-      const child = nextIsRight
+      const next = nextIsRight
         ? this.getRightChildIndex(cur)
         : this.getLeftChildIndex(cur);
 
-      if (this.compare(this.heap[cur], this.heap[child]) < 0) {
+      if (this.compareFn(this.heap[cur], this.heap[next]) <= 0) {
         break;
       }
 
-      this.swap(cur, child);
-      cur = child;
+      this.swap(cur, next);
+      cur = next;
     }
   }
-  top() {
-    return this.heap[0] ?? null;
-  }
-  isEmpty() {
-    return this.heap.length === 0;
-  }
+
   swap(a, b) {
     [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
   }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+
+  size() {
+    return this.heap.length;
+  }
 }
 
-function solution(input) {
+function solution(N, M, problemList, commandList) {
   const P = 0;
   const L = 1;
 
-  let cursor = 0;
-  const N = Number(input[cursor++]);
+  const result = [];
+
+  const unsolvedMap = new Map();
+  const solvedMap = new Map();
+
   const minHeap = new Heap((a, b) => a[L] - b[L] || a[P] - b[P]);
   const maxHeap = new Heap((a, b) => b[L] - a[L] || b[P] - a[P]);
-  const problemMap = new Map();
 
-  const trim = (heap) => {
-    while (!heap.isEmpty()) {
-      const [p, l] = heap.top();
-      if (problemMap.has(p) && problemMap.get(p) === l) {
+  const clearHeap = (heap, solvedMap) => {
+    while (true) {
+      const [problem, level] = heap.top();
+      const solvedSet = solvedMap.get(problem);
+      const notSolved = !solvedSet?.has(level);
+
+      if (notSolved) {
         break;
       }
 
@@ -105,37 +126,49 @@ function solution(input) {
     }
   };
 
-  while (cursor < N + 1) {
-    const [p, l] = input[cursor++].trim().split(" ").map(Number);
-    minHeap.push([p, l]);
-    maxHeap.push([p, l]);
-    problemMap.set(p, l);
-  }
+  problemList.forEach(([problem, level]) => {
+    minHeap.push([problem, level]);
+    maxHeap.push([problem, level]);
+    unsolvedMap.set(problem, level);
+  });
 
-  const M = Number(input[cursor++]);
-  const answer = [];
-  while (cursor < N + M + 2) {
-    const [command, ...argv] = input[cursor++].trim().split(" ");
-
+  commandList.forEach(([command, arg1, arg2]) => {
     switch (command) {
       case "add":
-        const [p, l] = argv.map(Number);
-        minHeap.push([p, l]);
-        maxHeap.push([p, l]);
-        problemMap.set(p, l);
-        break;
-      case "recommend":
-        const heap = argv[0] === "1" ? maxHeap : minHeap;
-        trim(heap);
-        answer.push(heap.top()[P]);
+        minHeap.push([arg1, arg2]);
+        maxHeap.push([arg1, arg2]);
+        unsolvedMap.set(arg1, arg2);
         break;
       case "solved":
-        problemMap.delete(Number(argv[P]));
+        const level = unsolvedMap.get(arg1);
+        unsolvedMap.delete(arg1);
+
+        const solvedSet = solvedMap.get(arg1) ?? new Set();
+        solvedSet.add(level);
+        solvedMap.set(arg1, solvedSet);
+        break;
+      case "recommend":
+        const targetHeap = arg1 === 1 ? maxHeap : minHeap;
+        clearHeap(targetHeap, solvedMap);
+        const [problem] = targetHeap.top();
+        result.push(problem);
         break;
     }
-  }
+  });
 
-  return answer.join("\n");
+  return result.join("\n");
 }
 
-console.log(solution(input));
+const toNumber = (string) => parseInt(string, 10);
+
+const N = toNumber(input.shift());
+const problemList = input
+  .splice(0, N)
+  .map((problemInfo) => problemInfo.split(" ").map(toNumber));
+const M = toNumber(input.shift());
+const commandList = input
+  .splice(0, M)
+  .map((commandInfo) => commandInfo.split(" "))
+  .map(([command, ...args]) => [command, ...args.map(toNumber)]);
+
+console.log(solution(N, M, problemList, commandList));
