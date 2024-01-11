a18c14c17ed53bda8e885e4864cf2b6f3e491ab6
박유현 <puh0128@gmail.com>
2024-01-11 17:11:26 +0900

    [level 2] Title: 의상, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,9 @@
+function solution(clothes) {
+    const clothesCountMap = new Map();
+    clothes.forEach(([name, category]) => {
+        const prevCount = clothesCountMap.get(category) ?? 0;
+        clothesCountMap.set(category, prevCount + 1);
+    });
+    
+    return [...clothesCountMap].reduce((acc, [_, count]) => acc * (count + 1), 1) - 1;
+}
