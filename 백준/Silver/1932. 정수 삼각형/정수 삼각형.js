84b89747f2c54a38ce53bca489656f6550f4284c
박유현 <puh0128@gmail.com>
2024-02-18 16:11:36 +0900

    [Silver I] Title: 정수 삼각형, Time: 200 ms, Memory: 15244 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.js
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.js
@@ -3,18 +3,16 @@ const path = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
 const input = fs.readFileSync(path).toString().trim().split("\n");
 
 function solution(n, triangle) {
-  for (let height = 1; height < n; height++) {
-    for (let index = 0; index < triangle[height].length; index++) {
-      triangle[height][index] = Math.max(
-        triangle[height][index] + (triangle[height - 1][index - 1] ?? 0),
-        triangle[height][index] + (triangle[height - 1][index] ?? 0)
-      );
+  const dp = triangle.map((row) => [...row]);
+  for (let level = 1; level < n; level += 1) {
+    const { length } = dp[level];
+    for (let index = 0; index < length; index += 1) {
+      dp[level][index] += Math.max(dp[level - 1]?.[index - 1] ?? 0, dp[level - 1]?.[index] ?? 0);
     }
   }
-
-  return Math.max(...triangle[n - 1]);
+  return Math.max(...dp.at(-1));
 }
 
-const n = Number(input.shift());
-const triangle = input.map((row) => row.split(" ").map(Number));
+const n = Number(input.splice(0, 1));
+const triangle = input.map((line) => line.split(" ").map(Number));
 console.log(solution(n, triangle));
