3419edb7f07fc12db934f105b3b4252483a8311e
박유현 <puh0128@gmail.com>
2024-02-18 17:02:09 +0900

    [Gold III] Title: 줄 세우기, Time: 384 ms, Memory: 46980 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.js
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.js
@@ -2,41 +2,59 @@ const fs = require("fs");
 const path = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
 const input = fs.readFileSync(path).toString().trim().split("\n");
 
-function solution(N, M, edge) {
-  const path = [];
-  const inDegree = Array(N + 1).fill(0);
-  const graph = Array.from(Array(N + 1), () => []);
-  inDegree[0] = Infinity;
-  edge.forEach((row) => {
-    const [u, v] = row.trim().split(" ").map(Number);
-    inDegree[v] += 1;
-    graph[u].push(v);
+function solution(N, M, compareResults) {
+  const PADDING = 1;
+  const graph = Array.from(Array(N), () => []);
+  const indegrees = Array(N).fill(0);
+  compareResults.forEach(([studentA, studentB]) => {
+    graph[studentA - PADDING].push(studentB - PADDING);
+    indegrees[studentB - PADDING] += 1;
   });
 
-  const topologySort = () => {
-    let head = 0;
-    const queue = [];
-    inDegree.forEach((num, node) => {
-      if (num === 0) {
-        queue.push(node);
+  const result = [];
+  const queue = new Queue();
+  indegrees.forEach((indegree, node) => {
+    if (indegree === 0) {
+      queue.push(node);
+    }
+  });
+
+  while (!queue.isEmpty()) {
+    const cur = queue.pop();
+    result.push(cur + PADDING);
+
+    graph[cur].forEach((next) => {
+      indegrees[next] -= 1;
+      if (indegrees[next] === 0) {
+        queue.push(next);
       }
     });
-
-    while (queue.length - head) {
-      const cur = queue[head++];
-      path.push(cur);
-
-      for (const next of graph[cur]) {
-        if (inDegree[next] === 1) {
-          queue.push(next);
-        }
-        inDegree[next] = Math.max(0, inDegree[next] - 1);
-      }
-    }
-  };
-
-  topologySort();
-  return path;
+  }
+  return result.join(" ");
 }
+
+class Queue {
+  head = 0;
+  queue = [];
+
+  push(value) {
+    this.queue.push(value);
+  }
+
+  pop() {
+    return this.queue[this.head++];
+  }
+
+  isEmpty() {
+    return this.queue.length === this.head;
+  }
+}
+
 const [N, M] = input.shift().split(" ").map(Number);
-console.log(solution(N, M, input).join(" "));
+console.log(
+  solution(
+    N,
+    M,
+    input.map((line) => line.split(" ").map(Number))
+  )
+);
