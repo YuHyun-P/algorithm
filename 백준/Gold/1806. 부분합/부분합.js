a05dc4dd7b96ee56e1b5c3782e20c29016c907d4
박유현 <puh0128@gmail.com>
2024-02-23 15:24:41 +0900

    [Gold IV] Title: 부분합, Time: 216 ms, Memory: 16724 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.js
+++ 백준/Gold/1806. 부분합/부분합.js
@@ -2,27 +2,23 @@ const fs = require("fs");
 const path = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
 const input = fs.readFileSync(path).toString().trim().split("\n");
 
-function solution(S, sequence) {
-  let right = 0;
-  let sum = sequence[right];
-  let min = Infinity;
-  for (let left = 0; left < sequence.length; left++) {
-    while (sum < S && right < sequence.length) {
-      right += 1;
-      if (right !== sequence.length) {
-        sum += sequence[right];
-      }
+function solution(N, S, sequence) {
+  let minLength = Infinity;
+  let end = 1;
+  let acc = sequence[end - 1];
+  for (let start = 0; start < N; start += 1) {
+    while (acc < S && end < N) {
+      acc += sequence[end];
+      end += 1;
     }
-    if (right === sequence.length) {
-      break;
+    if (acc >= S) {
+      minLength = Math.min(minLength, end - start);
     }
-
-    min = Math.min(min, right - left + 1);
-    sum -= sequence[left];
+    acc -= sequence[start];
   }
-  return min !== Infinity ? min : 0;
+  return minLength === Infinity ? 0 : minLength;
 }
 
-const [N, S] = input.shift().trim().split(" ").map(Number);
-const sequence = input[0].trim().split(" ").map(Number);
-console.log(solution(S, sequence));
+const [N, S] = input.shift().split(" ").map(Number);
+const sequence = input[0].split(" ").map(Number);
+console.log(solution(N, S, sequence));
