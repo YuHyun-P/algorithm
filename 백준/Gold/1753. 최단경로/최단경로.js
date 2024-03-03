f9da4c3e35c5caed6602465f347013c1e1ec85f8
박유현 <puh0128@gmail.com>
2024-03-03 23:43:00 +0900

    [Gold IV] Title: 최단경로, Time: 1096 ms, Memory: 135604 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.js
+++ 백준/Gold/1753. 최단경로/최단경로.js
@@ -1,64 +1,107 @@
-const fs = require("fs");
-const path = process.platform === "linux" ? "/dev/stdin" : "./예제.txt";
-const input = fs.readFileSync(path).toString().trim().split("\n");
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : '예제.txt';
+const input = fs.readFileSync(path).toString().trim().split('\n');
 
-class PriorityQueue {
-  constructor(compare) {
+function solution(nVertex, nEdge, start, edges) {
+  const NO_CONNECTION = Infinity;
+  return getMinDistances(nVertex, start, createSingleEdgeGraph(nVertex, edges))
+    .map((v) => (v === NO_CONNECTION ? 'INF' : v))
+    .join('\n');
+
+  function getMinDistances(nVertex, start, graph) {
+    const DISTANCE = 1;
+
+    const asecendingDistance = (a, b) => a[DISTANCE] - b[DISTANCE];
+    const minHeap = new Heap(asecendingDistance);
+
+    const result = Array(nVertex + 1).fill(NO_CONNECTION);
+    result[start] = 0;
+
+    graph[start].forEach(([next, weight]) => {
+      result[next] = weight;
+      minHeap.push([next, weight]);
+    });
+
+    while (!minHeap.isEmpty()) {
+      const [node, distance] = minHeap.pop();
+
+      if (result[node] !== distance) {
+        continue;
+      }
+
+      graph[node]
+        .filter(([next, weight]) => weight + distance < result[next])
+        .forEach(([next, weight]) => {
+          const nextDistance = distance + weight;
+          result[next] = nextDistance;
+          minHeap.push([next, nextDistance]);
+        });
+    }
+
+    result.shift();
+    return result;
+  }
+
+  function createSingleEdgeGraph(V, edges) {
+    const result = Array.from(Array(V + 1), () => new Map());
+    edges.forEach(([nodeFrom, nodeTo, weight]) => {
+      const neighborhood = result[nodeFrom];
+      const prevWeight = neighborhood.get(nodeTo);
+      if (prevWeight !== undefined && weight > prevWeight) {
+        return;
+      }
+      neighborhood.set(nodeTo, weight);
+    });
+    return result.map((neighborhood) => [...neighborhood]);
+  }
+}
+
+class Heap {
+  constructor(compareFn) {
+    this.compareFn = compareFn;
     this.heap = [];
-    this.compare = compare;
   }
-  getLeftChildIndex(parent) {
-    return 2 * parent + 1;
-  }
-  getRightChildIndex(parent) {
-    return 2 * parent + 2;
-  }
-  getParentIndex(child) {
-    return Math.floor((child - 1) / 2);
-  }
-  hasLeftChild(parent) {
-    return this.getLeftChildIndex(parent) < this.size();
-  }
-  hasRightChild(parent) {
-    return this.getRightChildIndex(parent) < this.size();
-  }
-  hasParent(child) {
-    return 0 <= this.getParentIndex(child);
-  }
-  top() {
-    return this.heap[0] ?? null;
-  }
-  pop() {
-    if (this.isEmpty()) {
-      return null;
-    }
-    if (this.size() === 1) {
-      return this.heap.pop();
-    }
-    const value = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    this.heapifyDown();
-    return value;
-  }
+
   push(value) {
     this.heap.push(value);
     this.heapifyUp();
   }
+
+  pop() {
+    if (this.isEmpty()) return null;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const result = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown();
+    return result;
+  }
+
+  heapifyUp() {
+    let cur = this.heap.length - 1;
+    while (
+      this.hasParent(cur) &&
+      this.compareFn(this.heap[cur], this.heap[this.getParentIndex(cur)]) < 0
+    ) {
+      const parent = this.getParentIndex(cur);
+      this.swap(cur, parent);
+      cur = parent;
+    }
+  }
+
   heapifyDown() {
     let cur = 0;
     while (this.hasLeftChild(cur)) {
-      const nextIsRightChild =
+      const nextIsRight =
         this.hasRightChild(cur) &&
-        this.compare(
+        this.compareFn(
           this.heap[this.getRightChildIndex(cur)],
           this.heap[this.getLeftChildIndex(cur)]
         ) < 0;
 
-      const next = nextIsRightChild
-        ? this.getRightChildIndex(cur)
-        : this.getLeftChildIndex(cur);
+      const next = nextIsRight ? this.getRightChildIndex(cur) : this.getLeftChildIndex(cur);
 
-      if (this.compare(this.heap[cur], this.heap[next]) <= 0) {
+      if (this.compareFn(this.heap[cur], this.heap[next]) <= 0) {
         break;
       }
 
@@ -66,72 +109,41 @@ class PriorityQueue {
       cur = next;
     }
   }
-  heapifyUp() {
-    let cur = this.heap.length - 1;
-    while (
-      this.hasParent(cur) &&
-      this.compare(this.heap[cur], this.heap[this.getParentIndex(cur)]) < 0
-    ) {
-      const parent = this.getParentIndex(cur);
-      this.swap(cur, parent);
-      cur = parent;
-    }
+
+  getLeftChildIndex(parentIndex) {
+    return parentIndex * 2 + 1;
   }
+
+  getRightChildIndex(parentIndex) {
+    return parentIndex * 2 + 2;
+  }
+
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+
+  hasLeftChild(parentIndex) {
+    return this.getLeftChildIndex(parentIndex) < this.heap.length;
+  }
+
+  hasRightChild(parentIndex) {
+    return this.getRightChildIndex(parentIndex) < this.heap.length;
+  }
+
+  hasParent(childIndex) {
+    return this.getParentIndex(childIndex) >= 0;
+  }
+
+  swap(indexA, indexB) {
+    [this.heap[indexA], this.heap[indexB]] = [this.heap[indexB], this.heap[indexA]];
+  }
+
   isEmpty() {
-    return this.size() === 0;
-  }
-  size() {
-    return this.heap.length;
-  }
-  swap(a, b) {
-    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+    return this.heap.length === 0;
   }
 }
 
-function solution(V, K, edge) {
-  const WEIGHT = 1;
-  const [distance, graph] = init(V, K, edge);
-
-  const minHeap = new PriorityQueue((a, b) => a[WEIGHT] - b[WEIGHT]);
-  graph[K].forEach(([node, weight]) => {
-    distance[node] = weight;
-    minHeap.push([node, weight]);
-  });
-
-  while (!minHeap.isEmpty()) {
-    const [cur, curDistance] = minHeap.pop();
-    if (distance[cur] !== curDistance) {
-      continue;
-    }
-
-    for (const [next, nextWight] of graph[cur]) {
-      if (distance[next] < distance[cur] + nextWight) {
-        continue;
-      }
-
-      distance[next] = distance[cur] + nextWight;
-      minHeap.push([next, distance[next]]);
-    }
-  }
-
-  return distance
-    .slice(1)
-    .map((d) => (d === Infinity ? "INF" : `${d}`))
-    .join("\n");
-}
-
-function init(V, K, edge) {
-  const distance = Array(V + 1).fill(Infinity);
-  distance[K] = 0;
-
-  const graph = Array.from(Array(V + 1), () => new Map());
-  edge.forEach((row) => {
-    const [u, v, w] = row.trim().split(" ").map(Number);
-    graph[u].set(v, Math.min(graph[u].get(v) ?? Infinity, w));
-  });
-  return [distance, graph.map((adjacent) => [...adjacent.entries()])];
-}
-
-const [V, E] = input.shift().trim().split(" ").map(Number);
-const K = Number(input.shift().trim());
-console.log(solution(V, K, input));
+const [V, E] = input.shift().split(' ').map(Number);
+const K = Number(input.shift());
+const edges = input.map((line) => line.split(' ').map(Number));
+console.log(solution(V, E, K, edges));
