da9ac56a389d5b575a11e1680897509f16bc67ac
박유현 <puh0128@gmail.com>
2024-03-04 08:10:47 +0900

    [Silver I] Title: 트리 순회, Time: 116 ms, Memory: 9356 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.js
+++ 백준/Silver/1991. 트리 순회/트리 순회.js
@@ -1,34 +1,37 @@
-const fs = require("fs");
-const path = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
-const input = fs.readFileSync(path).toString().trim().split("\n");
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : '예제.txt';
+const input = fs.readFileSync(path).toString().trim().split('\n');
 
-function solution(N, edge) {
-  const tree = new Map(edge);
-  const preOrder = [];
-  const inOrder = [];
-  const postOrder = [];
+function solution(N, nodeInfos) {
+  const ROOT = 'A';
+  const EMPTY = '.';
 
-  const recur = (cur) => {
-    const [left, right] = tree.get(cur);
-    preOrder.push(cur);
-    if (left !== ".") {
-      recur(left);
+  const tree = new Map(nodeInfos.map(([root, left, right]) => [root, [left, right]]));
+  return [전위순회(ROOT, tree), 중위순회(ROOT, tree), 후위순회(ROOT, tree)].join('\n');
+
+  function 전위순회(root, tree) {
+    if (root === EMPTY) {
+      return '';
     }
-    inOrder.push(cur);
-    if (right !== ".") {
-      recur(right);
+    const [left, right] = tree.get(root);
+    return root + 전위순회(left, tree) + 전위순회(right, tree);
+  }
+  function 중위순회(root, tree) {
+    if (root === EMPTY) {
+      return '';
     }
-    postOrder.push(cur);
-  };
-  recur("A");
-
-  return [preOrder.join(""), inOrder.join(""), postOrder.join("")];
+    const [left, right] = tree.get(root);
+    return 중위순회(left, tree) + root + 중위순회(right, tree);
+  }
+  function 후위순회(root, tree) {
+    if (root === EMPTY) {
+      return '';
+    }
+    const [left, right] = tree.get(root);
+    return 후위순회(left, tree) + 후위순회(right, tree) + root;
+  }
 }
 
-const N = Number(input.shift().trim());
-const edge = input.map((row) => {
-  const [parent, ...children] = row.trim().split(" ");
-  return [parent, children];
-});
-
-console.log(solution(N, edge).join("\n"));
+const N = Number(input.shift());
+const nodeInfos = input.map((line) => line.split(' '));
+console.log(solution(N, nodeInfos));
